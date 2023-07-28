de69c0fc2e7e0891c1cd5a281490c53aba7760c0
Thunder-4 <thunder-4@Admin-4s-iMac.local>
2023-07-28 21:24:22 +0800

    Wordfind_app shineer uusgew

---
+++ wordfind_app/android/app/src/main/kotlin/com/example/wordfind_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wordfind_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ wordfind_app/lib/gradient_text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GradientText extends StatelessWidget {
+  const GradientText(this.text, this.size, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  GradientText('text', 'size');
+  }
+}
