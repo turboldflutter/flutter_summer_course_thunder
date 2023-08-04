67f3192ffa56a87fdb9731af9da344930629a4d8
turboldflutter <turbold.flutter@gmail.com>
2023-08-04 18:29:47 +0800

    ssh not deer dahij oruulaw

--- wordfind_app/lib/start_page.dart
+++ wordfind_app/lib/start_page.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:wordfind_app/gradient_text.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({super.key});
@@ -9,8 +9,40 @@ class StartPage extends StatelessWidget {
     return const Scaffold(
       backgroundColor: Color(0xFFFBF5F2),
       appBar: AppBar(
-        leading: IconButton(onPressed: (){Navigator.of(context).pop();}, )),
-      )
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: Image.asset('assets/arrow_back.png')),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: SizedBox(
+          height: 50.0,
+          child: Image(image: 'assets/game_logo.png'),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: AssetImage('assets/game_logo.png'),
+          fit: BoxFit,
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 50)),
+              Image.asset('assets/iCodeGuyHead.png'),
+              Padding(padding: EdgeInsets.only(top: 20)),
+              GradientText('Player Name', 20.0),
+              Padding(padding: EdgeInsets.only(top: 20))
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: StartButton('Start'),
     );
   }
+}
+
+class StartButton {
 }
