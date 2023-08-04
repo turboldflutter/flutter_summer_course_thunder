1ff8af3c766660468be0080803b318a45fe05887
Thunder-4 <thunder-4@Admin-4s-iMac.local>
2023-08-04 20:59:19 +0800

    start page ee hiij duusgaw

--- wordfind_app/lib/start_page.dart
+++ wordfind_app/lib/start_page.dart
@@ -52,6 +52,24 @@ class StartButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: 310,
+      height: 60,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Color(0xFFE86B02), Color(0xFFFA9541)],
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight),
+          borderRadius: BorderRadius.circular(25)),
+      child: ElevatedButton(
+        onPressed: (){},
+        child: Text('START', style: TextStyle(fontFamily: 'Nunito', fontSize: 24, fontWeight: FontWeight.w700),),
+        style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25))),
+      ),
+    );
   }
 }
