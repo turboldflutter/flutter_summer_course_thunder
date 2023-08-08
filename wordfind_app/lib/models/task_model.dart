9de673e84d66f364644bae4e01ca0724a38de634
turboldflutter <turbold.flutter@gmail.com>
2023-08-08 19:43:42 +0800

    task model file deer list bichij suraw

--- wordfind_app/lib/models/task_model.dart
+++ wordfind_app/lib/models/task_model.dart
@@ -1,1 +1,22 @@
 import 'char_model.dart';
+
+class TaskModel {
+  late String question;
+  late String pathImage;
+  late String answer;
+  bool isDone = false;
+  bool isFull = false;
+  List<CharModel> puzzles = [];
+  List<String> arrayButtons = [];
+
+  TaskModel(
+      {required this.pathImage,
+      required this.question,
+      required this.answer,
+      this.arrayButtons = const []});
+  void setWordFindChar(
+    List<CharModel> puzzles,
+  ) {
+    this.puzzles = puzzles;
+  }
+}
