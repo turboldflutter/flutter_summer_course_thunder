9749ac86ae9001e939af97e1245508e447b400d4
turboldflutter <turbold.fluttar@gmail.com>
2023-09-20 08:16:10 +0800

    6r hesgiig bolgow odoo 7g hiih uldlee

--- day_19_flutter/lib/main.dart
+++ day_19_flutter/lib/main.dart
@@ -1,38 +1,46 @@
+import 'package:day_17_flutter/provider/user_provider.dart';
 import 'package:day_17_flutter/screens/home_screen.dart';
 import 'package:day_17_flutter/screens/login_screen.dart';
 import 'package:day_17_flutter/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp();
-  runApp(MaterialApp(
-    theme: ThemeData.dark().copyWith(
-      scaffoldBackgroundColor: Color.fromRGBO(0, 0, 0, 1),
-      textTheme: Typography().white.apply(fontFamily: 'Rubik'),
-    ),
-    home: StreamBuilder(
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.active) {
-          if (snapshot.hasData) {
-            // Navigator.push(
-            //     context, MaterialPageRoute(builder: (context) =>
-              return  HomeScreen();
-          }
-          if (snapshot.hasError) {
-            // if this is true
-            return Center(child: Text('${snapshot.error}'));
-          }
-        } else if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-              child: CircularProgressIndicator(
-            color: Colors.white,
-          ));
-        }
-        return LoginScreen();
-      },
-      // stream: FirebaseAuth.instance.authStateChanges(),
-    ),
-  ));
+  runApp(
+    MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => UserProvider()),
+        ],
+        child: MaterialApp(
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: Color.fromRGBO(0, 0, 0, 1),
+            textTheme: Typography().white.apply(fontFamily: 'Rubik'),
+          ),
+          home: StreamBuilder(
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                if (snapshot.hasData) {
+                  // Navigator.push(
+                  //     context, MaterialPageRoute(builder: (context) =>
+                  return HomeScreen();
+                }
+                if (snapshot.hasError) {
+                  // if this is true
+                  return Center(child: Text('${snapshot.error}'));
+                }
+              } else if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                    child: CircularProgressIndicator(
+                  color: Colors.white,
+                ));
+              }
+              return LoginScreen();
+            },
+            // stream: FirebaseAuth.instance.authStateChanges(),
+          ),
+        )),
+  );
 }
